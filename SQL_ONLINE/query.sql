2ef075306b5bbe7592fb46ffe1ae5385fb08c451
Victor Pongnian <victor5114@users.noreply.github.com>
2020-07-01 17:30:16 +0200

    Update query.sql
    Optimise SQL queries

--- SQL_ONLINE/query.sql
+++ SQL_ONLINE/query.sql
@@ -1,24 +1,23 @@
 -- Query 1 : Indiquer par pays, le nombre de clients qui n’ont pas effectué de commandes.
-SELECT "Country", count("Country") FROM (
-    SELECT
-    custs."CustomerID",
-    custs."Country",
-    count(ords."OrderID") as order_count
-    FROM [Customers] AS custs
-    LEFT JOIN [Orders] AS ords ON ords."CustomerID" = custs."CustomerID"
-    GROUP BY custs."CustomerID", custs."Country" HAVING order_count = 0)
-GROUP BY country
-
-
+SELECT
+	Customers.country,
+    count(Customers.CustomerID) as qty
+FROM Customers
+LEFT JOIN Orders ON Customers.CustomerID=Orders.CustomerID
+WHERE Orders.CustomerID IS NULL
+GROUP BY Customers.country
+ORDER BY qty DESC;
 
 -- Query 2 : Indiquer l’id produit ainsi que la somme des quantités achetées pour ce produit.
-SELECT odets."ProductID", SUM(odets."Quantity") AS product_sum_quantity
-FROM [Orders] AS ords
-INNER JOIN [OrderDetails] AS odets ON ords."OrderID" = odets."OrderID"
-GROUP BY odets."ProductID"
+SELECT
+    odets."ProductID",
+    SUM(odets."Quantity") AS product_sum_quantity
+FROM [OrderDetails] AS odets
+GROUP BY odets."ProductID";
 
 -- Query 3 : Indiquer la moyenne des années de naissance des employés arrondi à l’entier le plus proche.
-SELECT ROUND(AVG(emp."BirthDate")) AS avg_age_of_employees FROM [Employees] AS emp
+SELECT ROUND(AVG(emp."BirthDate")) AS avg_age_of_employees
+FROM [Employees] AS emp;
 
 -- Query 4 : Indiquer les ville et leur pays respectif ainsi que leur nombre de clients dont le nombre de clients est strictement supérieur à 3.
-SELECT city, country, count(CustomerID) AS unique_cust FROM [Customers] GROUP BY city, country HAVING unique_cust > 3
+SELECT city, country, count(CustomerID) AS unique_cust FROM [Customers] GROUP BY city, country HAVING unique_cust > 3;
